3cae975a680b4d1472bf51e5ae5d23c4042f9811
Osam-Frimpong Schandorf, MD <44724187+osamfrimpong@users.noreply.github.com>
2023-09-11 15:43:43 +0000

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 def new_in_list(my_list, idx, element):
-    if(my_list is not None):
-        m_list = my_list.copy()
+    if my_list:
         if idx < 0 or idx >= len(my_list):
             return my_list
         else:
-            m_list[idx] = element
-            return m_list
+            new_list = my_list.copy()
+            new_list[idx] = element
+            return new_list
